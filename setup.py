9be02ad0246ddbe83c0aaa186bec8c29cf7ba58b
Test User <cjp.agostino@gmail.com>
2026-04-06 18:50:32 -0400

    fix: rename package to qstkl (qstk taken on PyPI)

--- setup.py
+++ setup.py
@@ -1,7 +1,7 @@
 from setuptools import setup, find_packages
 
 setup(
-    name="qstk",
+    name="qstkl",
     version="0.1.0",
     description="Quantum Semantic Toolkit: Quantum semantic methods powered by npcpy.",
     author="Christopher Agostino",
